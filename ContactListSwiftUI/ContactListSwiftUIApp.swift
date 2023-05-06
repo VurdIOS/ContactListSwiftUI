fb87ea6cfc19b76a18e13b45b0b745e6db271e71
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-05-06 11:54:13 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Камаль Атавалиев on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
